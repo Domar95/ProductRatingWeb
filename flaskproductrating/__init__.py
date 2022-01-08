7b3b9dc3b57593fe53c8d5a2afb5d6a3bca1d009
Domar95 <87876261+Domar95@users.noreply.github.com>
2022-01-08 16:26:42 +0100

    update

--- flaskproductrating/__init__.py
+++ flaskproductrating/__init__.py
@@ -5,8 +5,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'QZJTt6VHV8rAsk3hkMMo3Zn8XzDxinj7'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://doagteifqovnvg:9be65b09c4b7245bb63f3c5ce57e50b25782dac60e4abeaf170f9e7f56276683@ec2-52-208-145-55.eu-west-1.compute.amazonaws.com:5432/d4mbg7ruiehmb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://doagteifqovnvg:9be65b09c4b7245bb63f3c5ce57e50b25782dac60e4abeaf170f9e7f56276683@ec2-52-208-145-55.eu-west-1.compute.amazonaws.com:5432/d4mbg7ruiehmb'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
